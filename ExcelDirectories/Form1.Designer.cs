1ab106cff0f641006e917d3e1e0856239c746359
chrdek <cdekatrr@hotmail.com>
2023-08-19 15:56:36 +0300

    added all directories, source files to main

---
+++ ExcelDirectories/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace ExcelDirectories
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.spreadsheet1 = new Spire.Spreadsheet.Forms.Spreadsheet();
+            this.SuspendLayout();
+            // 
+            // treeView
+            // 
+            this.treeView.Location = new System.Drawing.Point(13, 76);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(272, 567);
+            this.treeView.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(272, 57);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Select Directory...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.SelectedPath = "C:\\Users\\standarduser\\Desktop";
+            // 
+            // spreadsheet1
+            // 
+            this.spreadsheet1.ActiveSheetIndex = 0;
+            this.spreadsheet1.HorizontalScrollBarVisibility = true;
+            this.spreadsheet1.Location = new System.Drawing.Point(291, 13);
+            this.spreadsheet1.Name = "spreadsheet1";
+            this.spreadsheet1.ScrollBarsVisibility = true;
+            this.spreadsheet1.SheetTabControlWidth = 400;
+            this.spreadsheet1.Size = new System.Drawing.Size(870, 630);
+            this.spreadsheet1.TabIndex = 3;
+            this.spreadsheet1.VerticalScrollBarVisibility = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1173, 655);
+            this.Controls.Add(this.spreadsheet1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private Spire.Spreadsheet.Forms.Spreadsheet spreadsheet1;
+    }
+}
+
---
+++ ExcelDirectories/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using Microsoft.Office.Core;
+using System.Runtime.Remoting.Messaging;
+using DSOFile;
+using Spire.Xls;
+using Spire.Xls.Core.Spreadsheet;
+
+
+namespace ExcelDirectories
+{
+    public partial class Form1 : Form
+    {
+
+        private string strFile="";
+        private string strPath = "./";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ListDirectory(TreeView treeView, string path)
+        {
+            treeView.Nodes.Clear();
+            path = strPath;
+            var rootDirectoryInfo = new DirectoryInfo(path);
+            treeView.Nodes.Add(CreateDirectoryNode(rootDirectoryInfo));
+        }
+
+        private static TreeNode CreateDirectoryNode(DirectoryInfo directoryInfo)
+        {
+            var directoryNode = new TreeNode(directoryInfo.Name);
+            foreach (var directory in directoryInfo.GetDirectories())
+                directoryNode.Nodes.Add(CreateDirectoryNode(directory));
+            foreach (var file in directoryInfo.GetFiles())
+                directoryNode.Nodes.Add(new TreeNode(file.Name));
+            return directoryNode;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListDirectory(treeView,strPath);
+            treeView.NodeMouseClick += TreeView_NodeMouseClick;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           DialogResult dres = folderBrowserDialog1.ShowDialog();
+
+            if (dres == DialogResult.OK)
+                strPath = folderBrowserDialog1.SelectedPath;
+           ListDirectory(treeView,strPath);
+        }
+
+
+        private void TreeView_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+
+            FileInfo newxls = new FileInfo(@"./" + e.Node.Text + ".xls");
+            FileStream fsInfo = newxls.Create();
+            strFile = newxls.FullName;
+            fsInfo.Close();
+
+            CreateXLSView(strFile);
+            if (!File.Exists(strFile)) throw new Exception();
+            //Uri fileUri = new Uri("file:///" + strFile);
+            spreadsheet1.LoadFromFile(strPath+"\\output.xls", true);
+            GC.Collect();
+        }
+
+
+        public void CreateXLSView(string inputName)
+        {
+            Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+            
+            if (xlApp == null) return;
+            Microsoft.Office.Interop.Excel.Workbook wb = xlApp.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
+            xlApp.Workbooks.Open(inputName, 0, false, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t",
+                true, false, 0, true, 1, 0);
+            Microsoft.Office.Interop.Excel.Worksheet ws = (Microsoft.Office.Interop.Excel.Worksheet)wb.Worksheets[1];
+
+            Microsoft.Office.Interop.Excel.Range xlsRange = ws.get_Range("A1","J1");
+
+            for (int i = 1; i <= xlsRange.Count; i++)
+            {
+                xlsRange.Cells[1, i].Interior.Color = Color.DarkGray;
+                xlsRange.Cells[1, i].Font.Bold = true;
+                xlsRange.Cells[1, i].Font.Size = 12;
+            }
+            xlsRange.Cells[1, 1].Value = "Dir. Name";
+            xlsRange.Cells[1, 2].Value = "File Name";
+            xlsRange.Cells[1, 3].Value = "File Extension";
+            xlsRange.Cells[1, 4].Value = "Date Created";
+            xlsRange.Cells[1, 5].Value = "Date Last Modified";
+            xlsRange.Cells[1, 6].Value = "File Size";
+            xlsRange.Cells[1, 7].Value = "Author Name";
+            xlsRange.Cells[1, 8].Value = "Company Name";
+            xlsRange.Cells[1, 9].Value = "Title";
+            xlsRange.Cells[1, 10].Value = "Subject";
+
+            List<string[]> outputxlsData = CreateDirDataList(DirSearch(strPath));
+
+            for (int p=2; p < outputxlsData.Count();  p++)
+            {
+                for (int r=1; r < 10; r++)
+                {
+                    xlsRange.Cells[p, r].Value = outputxlsData[p][r];
+                }
+            }
+
+            wb.SaveAs(strPath+"\\output.xls", XlFileFormat.xlOpenXMLWorkbook, Missing.Value,
+    Missing.Value, false, false, XlSaveAsAccessMode.xlNoChange,
+    XlSaveConflictResolution.xlUserResolution, true,
+    Missing.Value, Missing.Value, Missing.Value);
+            xlApp.Workbooks.Close();
+        }
+
+
+  
+        private static List<FileInfo> DirSearch(string sDir)
+        {
+            List<FileInfo> allfilesInfo = new List<FileInfo>();
+            
+
+            try
+            {
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    foreach (string f in Directory.GetFiles(d))
+                    {
+                        allfilesInfo.Add(new FileInfo(f));
+
+                    }
+                    DirSearch(d);
+                }
+
+            }
+
+            catch (System.Exception excpt)
+            {
+                MessageBox.Show(excpt.Message);
+                
+            }
+
+            return allfilesInfo;
+        }
+
+
+
+
+        public static List<string[]> CreateDirDataList(List<FileInfo> fInfoList) { 
+
+        string[] propertiesdocs = new string[10];
+        List<string[]> allFileinformation = new List<string[]>();
+
+                foreach (var file in fInfoList)
+                {
+                    if (file.Extension == ".pdf" || file.Extension == ".doc" ||
+                        file.Extension == ".docx" || file.Extension == ".xls" ||
+                        file.Extension == ".xlsx" || file.Extension == ".rtf")
+                    {
+                        DSOFile.OleDocumentProperties dsoProps = new DSOFile.OleDocumentProperties();
+                        dsoProps.Open(file.FullName);
+
+                        propertiesdocs[0] = file.DirectoryName ?? " ";
+                        propertiesdocs[1] = file.Name + file.Extension ?? " ";
+                        propertiesdocs[2] = file.Extension ?? " ";
+                        propertiesdocs[3] = file.CreationTime.ToString() ?? " ";
+                        propertiesdocs[4] = file.LastAccessTime.ToString() ?? " ";
+                        propertiesdocs[5] = (((float) file.Length/1024)/1024f).ToString("F3") + "MB" ?? " ";
+                        propertiesdocs[6] = dsoProps.SummaryProperties.Author ?? " ";
+                        propertiesdocs[7] = dsoProps.SummaryProperties.Subject ?? " ";
+                        propertiesdocs[8] = dsoProps.SummaryProperties.Company ?? " ";
+                        propertiesdocs[9] = dsoProps.SummaryProperties.Title ?? " ";
+                        allFileinformation.Add(new string[]
+                        {
+                            propertiesdocs[0], propertiesdocs[1], propertiesdocs[2], propertiesdocs[3],
+                        propertiesdocs[4], propertiesdocs[5], propertiesdocs[6], propertiesdocs[7], propertiesdocs[8], propertiesdocs[9]
+                        });
+                    }
+                    else
+                    {
+
+                        propertiesdocs[0] = file.DirectoryName ?? " ";
+                        propertiesdocs[1] = file.Name + file.Extension ?? " ";
+                        propertiesdocs[2] = file.Extension ?? " ";
+                        propertiesdocs[3] = file.CreationTime.ToString() ?? " ";
+                        propertiesdocs[4] = file.LastAccessTime.ToString() ?? " ";
+                        propertiesdocs[5] = (((float) file.Length/1024)/1024f).ToString("F3") + "MB" ?? " ";
+                        propertiesdocs[6] = "N/A";
+                        propertiesdocs[7] = "N/A";
+                        propertiesdocs[8] = "N/A";
+                        propertiesdocs[9] = "N/A";
+                        allFileinformation.Add(new string[]
+                        {
+                            propertiesdocs[0], propertiesdocs[1], propertiesdocs[2], propertiesdocs[3],
+                        propertiesdocs[4], propertiesdocs[5], propertiesdocs[6], propertiesdocs[7], propertiesdocs[8], propertiesdocs[9]
+                        });
+
+                }
+                }
+            return allFileinformation;
+        }
+            
+
+    }
+}
